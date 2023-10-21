d069b7b17adeda16126efe15df50a3aa6108d948
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2023-10-21 19:48:23 +0200

    Update and rename cos32.asm to 2.2_2.3.asm

--- 2.2_2.3.asm
+++ 2.2_2.3.asm
@@ -1,4 +1,4 @@
-; Przyk³ad wywo³ywania funkcji MessageBoxA i MessageBoxW
+; PrzykÂ³ad wywoÂ³ywania funkcji MessageBoxA i MessageBoxW
 .686
 .model flat
 extern _ExitProcess@4 : PROC
@@ -13,28 +13,28 @@ public _main
 	;dw 'z','a','j','m','u','j','e',' '
 	;dw '1','6',' ','b','i','t', 00F3H ,'w', 0
 	;tytul_Win1250 db 'Tekst w standardzie Windows 1250', 0
-	;tekst_Win1250 db 'Ka¿dy znak zajmuje 8 bitów', 0
+	;tekst_Win1250 db 'KaÂ¿dy znak zajmuje 8 bitÃ³w', 0
 
 	tytulZ_Unicode dw 'Z','n','a','k','i', 0
 	tekstZ_Unicode dw 'T','o', ' ', 'j','e','s','t',' ','r','a','k','i','e','t','a', 0D83Dh, 0DE80h, 10
 	dw 'i',' ', 'k', 'a', 'r', 'e', 't', 'k', 'a', 0D83Dh, 0DE91h, 0
 .code
 _main PROC
-	 ;push 0 ; sta³a MB_OK
-	 ;push OFFSET tytul_Win1250 ; adres obszaru zawieraj¹cego tytu³
-	 ;push OFFSET tekst_Win1250 ; adres obszaru zawieraj¹cego tekst
+	 ;push 0 ; staÂ³a MB_OK
+	 ;push OFFSET tytul_Win1250 ; adres obszaru zawierajÂ¹cego tytuÂ³
+	 ;push OFFSET tekst_Win1250 ; adres obszaru zawierajÂ¹cego tekst
 	; push 0 ; NULL
 	 ;call _MessageBoxA@16
 
 	 ;push 0 ; stala MB_OK
-	 ;push OFFSET tytul_Unicode ; adres obszaru zawieraj¹cego tytu³
-	 ;push OFFSET tekst_Unicode ; adres obszaru zawieraj¹cego tekst
+	 ;push OFFSET tytul_Unicode ; adres obszaru zawierajÂ¹cego tytuÂ³
+	 ;push OFFSET tekst_Unicode ; adres obszaru zawierajÂ¹cego tekst
 	 ;push 0 ; NULL
 	 ;call _MessageBoxW@16
 
 	 push 0 ; stala MB_OK
-	 push OFFSET tytulZ_Unicode ; adres obszaru zawieraj¹cego tytu³
-	 push OFFSET tekstZ_Unicode ; adres obszaru zawieraj¹cego tekst
+	 push OFFSET tytulZ_Unicode ; adres obszaru zawierajÂ¹cego tytuÂ³
+	 push OFFSET tekstZ_Unicode ; adres obszaru zawierajÂ¹cego tekst
 	 push 0 ; NULL
 	 call _MessageBoxW@16
 
